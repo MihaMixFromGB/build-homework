af6adfbfdec5be3823dd16a3977a3578de00ea14
mihamix@yandex.ru <mihamix@yandex.ru>
2025-07-29 11:08:20 +0700

    task 4

--- tasks/4/transformer.js
+++ tasks/4/transformer.js
@@ -2,5 +2,12 @@ import { walk } from 'estree-walker'
 
 // should return transformed ast
 export function transformer(ast) {
-  
+  walk (ast, {
+    enter(node) {
+      if (node.type === 'VariableDeclaration' && ['const', 'let'].includes(node.kind)) {
+        node.kind = 'var';
+      }
+    },
+  })
+  return ast;
 }
